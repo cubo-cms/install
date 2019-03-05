a76f84b3bc8cc591934d91c3ab152d2284307bd3
papiando (HOME) <info@papiando.com>
2019-03-05 19:20:45 -0400

    Move IP tracker

--- js/iptracker.js
+++ js/iptracker.js
@@ -1,10 +1,8 @@
-
 var ipAPI="http://ip-api.com/json/?callback=?";
 
 function getCookie(name) {
 	var regex = new RegExp("/^(?:.*;)?[ ]{0,1}"+name+"\s*=\s*([^;]+)(?:.*)?$/");
 	var result = (document.cookie.match(regex)||[,null])[1];
-	alert(result);
 }
 
 $(document).ready(function() {
@@ -12,7 +10,6 @@ $(document).ready(function() {
 	if(!ipTracker) {
 		$.getJSON(ipAPI,function(json) {
 			Cookies.set('cubo-iptracker',json,{ expires: 7, path: '/' });
-			alert('Called API!!!');
 		});
 	}
 });
